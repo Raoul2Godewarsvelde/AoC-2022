00e29e1941ffa7c149d53468983826f982995e19
Raoul de Godewarsvelde <raouldegodewarsvelde.thetrueone@gmail.com>
2022-12-06 19:25:07 +0100

    OK: initialize ArrayList<ArrayList<Integer>> instructions + values

--- src/com/moko/aoc/day5/SupplyStacks.java
+++ src/com/moko/aoc/day5/SupplyStacks.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+import static java.lang.Integer.parseInt;
+
 public class SupplyStacks {
 
-    public ArrayList<Stack<String>> initialzeStacks(int width, int height) {
-        ArrayList<Stack<String>> stacks = new ArrayList<>(9);
-        for (int i = 0; i <= 9; i++){
+    public ArrayList<Stack<String>> initializeStacks(int width) {
+        ArrayList<Stack<String>> stacks = new ArrayList<>(width);
+        for (int i = 0; i <= width; i++){
             Stack<String> stack = new Stack<>();
             stacks.add(stack);
         }
@@ -20,13 +22,12 @@ public class SupplyStacks {
         try {
             File file = new File("assets/texts/day5.txt");
             Scanner scanner = new Scanner(file);
-
             boolean allDataFetched = false;
-            ArrayList<Stack<String>> stacks = initialzeStacks(9, 8);
-
+            ArrayList<Stack<String>> stacks = initializeStacks(9);
+            ArrayList<ArrayList<Integer>> instructions = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
-                if(!allDataFetched && data.length() > 0 && data.charAt(0) != ' ') {
+                if (!allDataFetched && data.length() > 0 && data.charAt(0) != ' ') {
                     System.out.println(data.length());
                     int j = 0;
                     for (int i = 1; i <= 33; i += 4) {
@@ -34,8 +35,22 @@ public class SupplyStacks {
                         j++;
                         System.out.println(stacks);
                     }
-                } else {
+                } else if (!allDataFetched && data.length() == 0) {
                     allDataFetched = true;
+                } else if (data.length() > 0 && data.charAt(0) != ' ') {
+                    ArrayList<Integer> instruction = new ArrayList<>();
+                    instructions.add(instruction);
+                    boolean isNumber = false;
+                    StringBuilder value = new StringBuilder();
+                    for (int i = 0; i < data.length(); i++) {
+                        if (data.charAt(i) == ' ') isNumber = !isNumber;
+                        else if (isNumber) value.append(data.charAt(i));
+                        if ((value.length() > 0 && !isNumber) || i == data.length() - 1) {
+                            instructions.get(instructions.size() - 1).add(parseInt(String.valueOf(value)));
+                            value = new StringBuilder();
+                        }
+                    }
+                    System.out.println(instructions);
                 }
             }
             System.out.println("Day 5 (part1): ");
